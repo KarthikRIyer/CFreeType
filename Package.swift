0f47fb1970769130540bc5ebf79254eddee0aef7
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2019-08-16 11:06:23 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     providers: [.brew(["freetype2"]), .apt(["libfreetype6-dev"])],
     products: [
         .library(name: "CFreeType", targets: ["CFreeType"])
-    ],
+    ]
     // targets: [
     //     .systemLibrary(name: "CFreeType",
     //                    path: "Sources/CFreeType",
